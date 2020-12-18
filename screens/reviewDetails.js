93c2d3c287c82cdf012ae92a04504ed224ebf9bb
Brandon Lai <brandon.lai@vanderbilt.edu>
2020-12-18 01:01:18 -0600

    adds review details page to navigation stack

--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 import { globalStyles } from '../styles/global';
 
-export default function reviewDetails() {
+export default function reviewDetails({ navigation }) {
+    const pressHandler = () => {
+        navigation.goBack();
+    }
+
     return (
        <View style={globalStyles.container}>
            <Text style={globalStyles.titleText}>Review Details Screen</Text>
+           <Button title='back to home screen' onPress={pressHandler} />
        </View> 
     )
 }
